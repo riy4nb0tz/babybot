8e482188b87fecb910db383c6b2e2aed27996778
riy4n_botz <86124000+riy4nb0tz@users.noreply.github.com>
2021-07-10 07:59:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('MIKABOT WHATSAPP', {
+CFonts.say('ARDBOT WHATSAPP', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`Welcome MIKACHAN`, {
+CFonts.say(`Welcome ARDBOT`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
